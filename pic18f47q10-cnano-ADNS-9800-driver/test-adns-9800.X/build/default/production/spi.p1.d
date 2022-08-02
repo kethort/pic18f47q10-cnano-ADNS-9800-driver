5b9e9afa90d514cb02f67f146251cd30e426dc0e
kethort <10733500+kethort@users.noreply.github.com>
2022-08-01 22:27:43 -0700

    removed build dirs

--- pic18f47q10-cnano-ADNS-9800-driver/test-adns-9800.X/build/default/production/spi.p1.d
+++
@@ -1,9 +0,0 @@
-build/default/production/spi.p1:  \
-spi.c  \
-spi.h  \
-amiga_mouse_config.h  \
-mcc_generated_files/mcc.h  \
-mcc_generated_files/device_config.h  \
-mcc_generated_files/pin_manager.h  \
-mcc_generated_files/interrupt_manager.h  \
-mcc_generated_files/eusart2.h 
